c91e0a91532f792eab7cd7f3e6786d956b10072b
Lucas Hiago <lucashiago63@gmail.com>
2019-03-24 13:28:42 +0000

    Update second task
    New genetic code

--- second_task.py
+++ second_task.py
@@ -1,40 +1,49 @@
+
 geneticCode = {
-    'UUU': 'F', 'UUF': 'F', 'UUA': 'L', 'UUG': 'L',  # UU
-    'UCU': 'S', 'UCC': 'S', 'UCA': 'L', 'UCG': 'L',  # UC
-    'UAU': 'T', 'UAC': 'T', 'UAA': 'ST', 'UAG': 'ST',  # UA
-    'UGU': 'C', 'UGC': 'C', 'UGA': 'ST', 'UGG': 'W',  # UG
-    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
-    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
-    'CAU': 'H', 'CAC': 'H', 'CAA': 'G', 'CAG': 'G',
-    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
-    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
-    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
-    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
-    'AGU': 'S', 'AGC': 'S', 'AGA': 'A', 'AGG': 'A',
-    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
-    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
-    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
-    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+'UUU':'F', 'UUC':'F', 'UUA':'L', 'UUG':'L',
+'UCU':'S', 'UCC':'S', 'UCA':'L', 'UCG':'L',
+'UAU':'Y', 'UAC':'Y', 'UAA':'ST', 'UAG':'ST',
+'UGU':'C', 'UGC':'C', 'UGA':'ST', 'UGG':'W',
+'CUU':'L', 'CUC':'L', 'CUA':'L', 'CUG':'L',
+'CCU':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+'CAU':'H', 'CAC':'H', 'CAA':'Q', 'CAG':'Q',
+'CGU':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R',
+'AUU':'I', 'AUC':'I', 'AUA':'I', 'AUG':'M',
+'ACU':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
+'AAU':'N', 'AAC':'N', 'AAA':'K', 'AAG':'K',
+'AGU':'S', 'AGC':'S', 'AGA':'R', 'AGG':'R',
+'GUU':'V', 'GUC':'V', 'GUA':'V', 'GUG':'V',
+'GCU':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+'GAU':'D', 'GAC':'D', 'GAA':'E', 'GAG':'E',
+'GGU':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G'
 }
+
 
 #Dupla: Juliana Carla e Lucas Hiago
 
+'''
+O código foi testado com os seguintes I/O:
+Input: "AUGGUCUACAUAGCUGACAAACAGCACGUAGCAUCUCGAGAGGCAUAUGGUCACAUGUUCAAAGUUUGCGCCUAG"
+Output: "MVYIADKQHVASREAYGHMFKVCA"
+'''
+
 RNAm = input()
-'''
-O programa foi testado com este input
-RNAm = "GGC.CGA.UUA.AUG.CUU.AAA.UGC.GGC.CUA.AAU.UAU"
-'''
-RNAm = RNAm.split(".") #para separar dos pontos os codons
+i = start = 0
+j = 3
+codons = []
+while 1:
+	if RNAm[i:j] == "UAA" or RNAm[i:j] == "UGA" or RNAm[i:j] == "UAG":
+		break
+	else:
+		if RNAm[i:j] == "AUG":
+			start = 1
+		if start == 1:
+			codons.append(RNAm[i:j])
+		i = j
+		j += 3
+
 aminoacido = ""
-start = 0
-for codon in RNAm:
-    if codon == "AUG":
-        #significa que vai iniciar a sintese
-        aminoacido += geneticCode[codon] + " "
-        start = 1
-    elif codon == "UAA" or codon == "UAG" or codon == "UGA":
-        #siginifca que vai parar
-        break
-    elif start:
-        aminoacido += geneticCode[codon] + " "
-print("Cadeia de Aminoácido " + aminoacido)
+for codon in codons:
+	aminoacido += geneticCode[codon]
+print(aminoacido)
+
