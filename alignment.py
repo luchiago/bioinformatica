4d98d59cf4fc997798a55dfe56d952c3947d7c9e
Lucas Hiago <lucashiago63@gmail.com>
2019-05-08 16:53:23 -0300

    Traceback iniciado

--- alignment.py
+++ alignment.py
@@ -1,8 +1,7 @@
-import numpy as np
 
-gap = -10
-match = 5
-mismatch = -3
+gap = -2
+match = 1
+mismatch = -1
 
 def build_table(v, h):
     table = []
@@ -23,11 +22,88 @@ def build_table(v, h):
 
     return table
 
+
+def build_table_of_dir(v, h):
+    table = []
+
+    for i in range(0, v):
+        row = []
+        for j in range(0, h):
+            row.append(None)
+        table.append(row)
+
+    return table
+
+
+def traceback(v, h, table, table_of_dir):
+    
+    major = -10000000000000000
+    tam_v = len(v)
+    tam_h = len(h)
+
+    for i in range(0, len(v)):
+        if table[i][tam_h - 1] > major:
+            major = table[i][tam_h - 1]
+    
+    for i in range(0, len(h)):
+        if table[0][i] > major:
+            major = table[0][i]
+    
+    print(major)
+
 def alignment(t, v, h, table):
     
     tam_v = len(v)
     tam_h = len(h)
 
+    table_of_dir = build_table_of_dir(tam_v, tam_h)
+
+    letter_v = 3
+    for i in range(tam_v - 2, -1, -1):
+        letter_h = 1
+        for j in range(1, tam_h):
+            sup = table[i][j - 1] + gap
+            low = table[i + 1][j] + gap
+            mid = table[i + 1][j - 1]
+
+            if v[letter_v] == h[letter_h]:
+                mid += match
+            elif v[letter_v] != h[letter_h]:
+                mid += mismatch 
+
+            choose_one = max(sup, low, mid)
+
+            if choose_one == sup:
+                #esquerda
+                direction = [[i, j - 1], [v[letter_v], h[letter_h]]]
+            elif choose_one == low:
+                #abaixo
+                direction = [[i + 1, j], [v[letter_v], h[letter_h]]]
+            elif choose_one == mid:
+                #diagonal
+                direction = [[i + 1, j - 1], [v[letter_v], h[letter_h]]]
+
+            if t == 1 and choose_one < 0:
+                #se for local
+                choose_one = 0
+            table[i][j] = choose_one
+            table_of_dir[i][j] = direction
+            
+            letter_h += 1
+        letter_v -= 1
+    '''
+    for i in range(0, tam_v):
+        for j in range(0, tam_h):
+            print(table[i][j], end=' ')
+        print('\n')
+    
+    for i in range(0, tam_v):
+        for j in range(0, tam_h):
+            print(table_of_dir[i][j], end=' ')
+        print('\n')
+    '''
+
+    traceback(v, h, table, table_of_dir)
     
 
 if __name__ == "__main__":
